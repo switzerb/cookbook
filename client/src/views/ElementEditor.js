ace4c81540bf7c5ac16e65168291c718928d4549
switzerb <brenna.switzer@gmail.com>
2019-08-11 11:45:32 -0700

    Bypassing react state in favor of workaround

--- client/src/views/ElementEditor.js
+++ client/src/views/ElementEditor.js
@@ -14,15 +14,47 @@ class ElementEditor extends Component<{}> {
   constructor(props) {
     super(props)
     this.input = null
-    this.state = {
-      showSuggestions: true,
+    this.immediateState = {
       raw: this.props.raw || ''
     }
-  
     // AMOUNT, UNIT, NEW_UNIT, NEW_ITEM, ITEM there are five types of ranges
+  }
   
-    // this is the data that comes back from recognize element
-    this.stuff = {
+  componentDidMount() {
+    const raw = this.props.raw || ''
+    
+    this.immediateState = {
+      raw,
+      caret: Number.isFinite(this.props.caret) ? this.props.caret : raw.length,
+    }
+    this.setCaretPosition()
+  }
+  
+  setCaretPosition = () => {
+    const rawLength = this.immediateState.raw != null && this.immediateState.raw.length
+
+    const newCaretPosition =
+      this.immediateState.caret < rawLength
+        ? this.immediateState.caret
+        : rawLength
+    const currentCaretPosition = this.caret.getPosition({avoidFocus: true})
+
+    if (currentCaretPosition !== -1) {
+      this.caret.setPosition(newCaretPosition >= 20 ? newCaretPosition : -1)
+    }
+  }
+  
+  handleCaretMove = e => {
+    const caret = this.caret.getPosition()
+    if ((caret !== this.immediateState.caret)) {
+      this.immediateState.caret = caret
+      this.requestData()
+    }
+  }
+  
+  requestData = () => {
+  
+    const MOCK = {
       "suggestions": [
         {
           "target": {
@@ -50,87 +82,39 @@ class ElementEditor extends Component<{}> {
       ],
       "raw": "3 1/2 _Tbsp_ glerg"
     }
-  }
   
-  componentDidMount() {
-    const raw = this.props.raw || ''
-    
-    this.immediateState = {
-      raw,
-      caret: Number.isFinite(this.props.caret) ? this.props.caret : raw.length,
-    }
   
-    //request styling
-    this.setCaretPosition()
-  }
-  
-  setCaretPosition = () => {
-    const rawLength = this.immediateState.raw != null && this.immediateState.raw.length
-    
-    const newCaretPosition =
-      this.immediateState.caret < rawLength
-        ? this.immediateState.caret
-        : rawLength
-    const currentCaretPosition = this.caret.getPosition({avoidFocus: true})
-    
-    if (currentCaretPosition !== -1) {
-      this.caret.setPosition(newCaretPosition >= 0 ? newCaretPosition : -1)
-    }
-  }
-  
-  handleInput = () => {
-    const raw = this.getRaw()
-  }
-  
-  handleCaretMove = e => {
-    const caret = this.caret.getPosition()
-    
-    if ((caret !== this.immediateState.caret)) {
-      this.immediateState.caret = caret
-      this.requestData()
-    }
-  }
-  
-  requestData = () => {
     const raw = this.getRaw()
     RecipeApi.recognizeElement(raw).then( recognized => {
         // transform DOM style here
-        this.transformStyle(recognized)
+        if (this.getRaw() === recognized.raw) {
+          this.transformStyle(recognized)
+        }
       }
     )
   }
   
   transformStyle(recognized) {
     const { raw, ranges } = recognized;
-    
-    // console.log( raw.substring(nr.start, nr.end))
-    //   return this.setState({
-    //     raw,
-    //     q: qr && raw.substring(qr.start, qr.end),
-    //     u: ur && raw.substring(ur.start, ur.end),
-    //     n: nr && raw.substring(nr.start, nr.end),
-    //     p: recog.ranges.reduce(
-    //       (
-    //         p,
-    //         r,
-    //       ) => p.replace(recog.raw.substring(r.start, r.end), ''),
-    //       recog.raw,
-    //     ),
-    //   })
-    //
-    // const styledraw =  Array.from(raw).map((letter, index) => {
-    //   const className = "thing"
-    //
-    //   return (
-    //     <span
-    //       // eslint-disable-next-line react/no-array-index-key
-    //       key={index + letter}
-    //       className={className}
-    //     >{letter === ' ' ? '\u00a0' : letter}</span>
-    //   )
-    // })
-    this.setState({ raw: raw })
   
+    const styled = Array.from(raw).map((letter, index) => {
+      const className = "thing"
+      return (
+        <span
+          // eslint-disable-next-line react/no-array-index-key
+          key={index + letter}
+          className={className}
+        >{letter === ' ' ? '\u00a0' : letter}</span>
+      )
+    })
+  
+    const props = {
+      raw: styled,
+      caret: this.caret.getPosition()
+    }
+  
+    this.immediateState = props
+    this.forceUpdate()
   }
   
   getRaw() {
@@ -147,26 +131,27 @@ class ElementEditor extends Component<{}> {
   };
   
   handleKeyUp = e => {
-    const props = {raw: this.getRaw()}
-    
+    const props = {
+      raw: this.getRaw(),
+      caret: this.caret.getPosition()
+    }
+
     if (this.immediateState.raw === props.raw) {
       this.handleCaretMove(e)
       return
     }
-    
+  
     this.immediateState = props
     this.requestData()
   }
   
   render() {
-    const {showSuggestions, raw} = this.state
-    console.log(this.props);
+    const {raw} = this.immediateState
     
     return (
       <div>
         <ContentEditable
           ref={this.inputRef}
-          onInput={this.handleInput}
           onComponentUpdate={this.setCaretPosition}
           onKeyUp={this.handleKeyUp}
           onClick={this.handleCaretMove}
