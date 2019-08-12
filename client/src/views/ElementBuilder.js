c44554878ad1c20e73efb08022560a9a60082ff7
switzerb <brenna.switzer@gmail.com>
2019-08-11 17:11:19 -0700

    Add SVG inline library

---
+++ client/src/views/ElementBuilder.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import QueryAssist from "../vendor/ringui/components/query-assist/query-assist"
+
+
+const dataSource = ({query, caret}) => ({
+  query,
+  caret,
+  styleRanges: [
+    {start: 0, length: 1, style: 'text'},
+    {start: 1, length: 1, style: 'field_value'},
+    {start: 2, length: 1, style: 'field_name'},
+    {start: 3, length: 1, style: 'operator'}
+  ],
+  suggestions: [{
+    prefix: 'login: ',
+    option: 'test',
+    suffix: ' ',
+    description: '1',
+    matchingStart: 0,
+    matchingEnd: query.length,
+    caret: 2,
+    completionStart: 0,
+    completionEnd: query.length,
+    group: 'Recipes'
+  }, {
+    prefix: 'login: ',
+    option: 'test.1',
+    suffix: ' ',
+    description: '2',
+    matchingStart: 0,
+    matchingEnd: query.length,
+    caret: 2,
+    completionStart: 0,
+    completionEnd: query.length,
+    group: 'Ingredients'
+  }, {
+    prefix: 'name: ',
+    option: 'another',
+    suffix: ' ',
+    description: '2',
+    matchingStart: 0,
+    matchingEnd: query.length,
+    caret: 2,
+    completionStart: 0,
+    completionEnd: query.length,
+    group: 'Names'
+  }]
+});
+
+const ElementBuilder = () => {
+  
+  return (
+    <div>
+      <QueryAssist
+        placeholder="placeholder"
+        onApply={console.log}
+        dataSource={dataSource}
+      />
+    </div>
+  )
+}
+
+export default ElementBuilder
