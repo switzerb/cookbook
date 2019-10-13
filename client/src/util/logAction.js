9d05d7d839558e75259f12875a9ae0b65cc50e61
Barney Boisvert <bboisvert@gmail.com>
2019-10-13 09:38:27 -0700

    provide white/blacklisting for `logAction`

--- client/src/util/logAction.js
+++ client/src/util/logAction.js
@@ -1,4 +1,22 @@
+// If this is non-empty, only action types containing one of the listed strings
+// will be logged.
+const whitelist = [
+]
+
+// If whitelist is empty, action types containing any of the listed strings will
+// NOT be logged.
+const blacklist = [
+    "route/",
+    "temporal/",
+    "window/",
+]
+
 const logAction = action => {
+    if (whitelist && whitelist.length > 0) {
+        if (whitelist.every(it => action.type.indexOf(it) < 0)) return
+    } else if (blacklist && blacklist.length > 0) {
+        if (blacklist.some(it => action.type.indexOf(it) >= 0)) return
+    }
     const temp = {...action}
     delete temp.type
     const keys = Object.keys(temp)
@@ -10,6 +28,7 @@ const logAction = action => {
     } else {
         args.push(temp)
     }
+    // eslint-disable-next-line no-console
     console.log(...args)
 }
 
