ea6a9c82720eea481dba6bcaa8c957f3c7b57b67
Barney Boisvert <bboisvert@gmail.com>
2021-04-25 08:20:47 -0700

    make constraints explicit

--- src/main/java/com/brennaswitzer/cookbook/sudoku/AC3Solver.java
+++ src/main/java/com/brennaswitzer/cookbook/sudoku/AC3Solver.java
@@ -15,7 +15,18 @@ public class AC3Solver extends Sudoku {
     }
 
     @Value
-    private static class Arc {
+    private static class Constraint {
+
+        int x;
+
+        @EqualsAndHashCode.Exclude
+        @ToString.Exclude
+        Predicate<Integer> constraint;
+
+    }
+
+    @Value
+    private static class BiConstraint {
 
         int x, y;
 
@@ -26,27 +37,37 @@ public class AC3Solver extends Sudoku {
     }
 
     private BitSet[] domains;
-    private Bag<Arc>[] inboundArcs;
 
     protected boolean solve() {
         // variables are implicit: [0-len)
-        buildDomains();
-        applyGivens();
-        buildArcs();
-        UniqueBag<Arc> queue = new UniqueBag<>();
-        for (Bag<Arc> arcs : inboundArcs) {
-            for (Arc a : arcs) {
+        domains = buildDomains();
+        for (Constraint c : buildUnaryConstraints()) {
+            constrainDomain(domains[c.x], c.constraint);
+        }
+        //noinspection unchecked
+        Bag<BiConstraint>[] inboundArcs = (Bag<BiConstraint>[]) new Bag[len];
+        for (int c = 0; c < len; c++) {
+            Bag<BiConstraint> arcs = new Bag<>();
+            inboundArcs[c] = arcs;
+        }
+        for (BiConstraint c : buildBinaryConstraints()) {
+            inboundArcs[c.y].push(c);
+        }
+
+        UniqueBag<BiConstraint> queue = new UniqueBag<>();
+        for (Bag<BiConstraint> arcs : inboundArcs) {
+            for (BiConstraint a : arcs) {
                 queue.push(a);
             }
         }
         while (!queue.isEmpty()) {
             enterFrame();
-            Arc arc = queue.pop();
+            BiConstraint arc = queue.pop();
             if (arcReduce(arc)) {
                 if (domains[arc.x].isEmpty()) {
                     throw new IllegalArgumentException("Inconsistent/unsolvable problem?!");
                 }
-                for (Arc a : inboundArcs[arc.x]) {
+                for (BiConstraint a : inboundArcs[arc.x]) {
                     if (a.x != arc.y) {
                         queue.push(a);
                     }
@@ -69,7 +90,7 @@ public class AC3Solver extends Sudoku {
         return solved;
     }
 
-    private boolean arcReduce(Arc arc) {
+    private boolean arcReduce(BiConstraint arc) {
         BitSet Dx = domains[arc.x];
         BitSet Dy = domains[arc.y];
         return constrainDomain(Dx, vx ->
@@ -88,36 +109,37 @@ public class AC3Solver extends Sudoku {
         return changed;
     }
 
-    private void buildArcs() {
-        //noinspection unchecked
-        inboundArcs = (Bag<Arc>[]) new Bag[len];
+    private Bag<BiConstraint> buildBinaryConstraints() {
+        Bag<BiConstraint> cons = new Bag<>();
         for (int c = 0; c < len; c++) {
-            Bag<Arc> arcs = new Bag<>();
-            inboundArcs[c] = arcs;
             for (int n : getNeighbors(c)) {
-                arcs.push(new Arc(n, c, (a, b) -> !a.equals(b)));
+                cons.push(new BiConstraint(c, n, (a, b) -> !a.equals(b)));
             }
         }
+        return cons;
     }
 
-    private void applyGivens() {
+    private Bag<Constraint> buildUnaryConstraints() {
+        Bag<Constraint> cons = new Bag<>();
         for (int c = 0; c < len; c++) {
             if (board[c] != EMPTY_CELL) {
                 int n = board[c];
-                constrainDomain(domains[c], i -> i.equals(n));
+                cons.push(new Constraint(c, i -> i.equals(n)));
             }
         }
+        return cons;
     }
 
-    private void buildDomains() {
+    private BitSet[] buildDomains() {
         BitSet seed = new BitSet(dim);
         for (int i = 1; i <= dim; i++) {
             seed.set(i);
         }
-        domains = new BitSet[len];
+        BitSet[] domains = new BitSet[len];
         for (int i = 0; i < len; i++) {
             domains[i] = (BitSet) seed.clone();
         }
+        return domains;
     }
 
 }
